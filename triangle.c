2dbea22a52e826f8a0a86aae05366c4e83afedf9
Kuznecoff <artem98kuznecov@mail.ru>
2018-04-08 00:53:05 +0500

    1 hw correction

--- triangle.c
+++ triangle.c
@@ -6,7 +6,9 @@ int main(){
 	float b;
 	float c;
 	scanf("%f %f %f", &a, &b, &c);
-	if (fabsf(a*a - b*b - c*c) < 0.001 || fabsf(b*b - a*a - c*c) < 0.001 || fabsf(c*c - a*a - b*b) < 0.001 )
+	if (a >= b + c || b >= a + c || c >= a + b)
+		printf("Not valid\n");
+	else if (fabsf(a*a - b*b - c*c) < 0.001 || fabsf(b*b - a*a - c*c) < 0.001 || fabsf(c*c - a*a - b*b) < 0.001 )
 		printf("True\n");
 	else
 		printf("False\n");
